23b1c9af54b3b9ab658122d4ae51cc489e07e178
kalam04 <kalamgbcse@gmail.com>
2025-07-01 11:34:43 +0600

    add log in file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
 function runFunction() {
-    console.log("Hello, Node.js!");
+    const message = `Hello, Node.js! - ${new Date().toISOString()}\n`;
+
+    console.log("Hello, Node.js!"); // Still logs to terminal
+    fs.appendFileSync(path.join(__dirname, 'run.log'), message);
 }
 
 module.exports = {
